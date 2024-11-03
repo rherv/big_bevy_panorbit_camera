b3ed573e8d3f1ecb44677c9d8439b002f7cc8835
rherv <137662855+rherv@users.noreply.github.com>
2024-11-03 10:46:08 -0800

    Update util.rs

--- src/util.rs
+++ src/util.rs
@@ -5,6 +5,7 @@ use big_space::ReferenceFrame;
 
 const EPSILON: f32 = 0.001;
 
+/*
 pub fn calculate_from_translation_and_focus(translation: Vec3, focus: Vec3) -> (f32, f32, f32) {
     let comp_vec = translation - focus;
     let mut radius = comp_vec.length();
@@ -18,6 +19,22 @@ pub fn calculate_from_translation_and_focus(translation: Vec3, focus: Vec3) -> (
     };
     let pitch = (comp_vec.y / radius).asin();
     (yaw, pitch, radius)
+}
+*/
+
+pub fn calculate_from_translation_and_focus(translation: DVec3, focus: DVec3) -> (f32, f32, f32) {
+    let comp_vec = translation - focus;
+    let mut radius = comp_vec.length();
+    if radius == 0.0 {
+        radius = 0.05; // Radius 0 causes problems
+    }
+    let yaw = if comp_vec.x == 0.0 && comp_vec.z >= 0.0 {
+        0.0
+    } else {
+        (comp_vec.z / (comp_vec.x.powi(2) + comp_vec.z.powi(2)).sqrt()).acos()
+    };
+    let pitch = (comp_vec.y / radius).asin();
+    (yaw as f32, pitch as f32, radius as f32)
 }
 
 /// Update `transform` based on yaw, pitch, and the camera's focus and radius
